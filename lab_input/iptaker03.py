fd310032aeaceb42f0a4c479102f2eaed2878c15
yaping <yaping0816@gmail.com>
2022-11-15 22:28:01 +0000

    completed lab 30, 33 and 34

---
+++ lab_input/iptaker03.py
@@ -0,0 +1,7 @@
+#!/user/bin/env python3
+
+def askuser():
+    name = input("what's your name?")
+    day = input("what's the day of week it is?")
+    print(f"Hello, {name}! Happy {day}!")
+askuser()
