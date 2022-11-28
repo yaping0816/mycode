e36f98c174b7d10cb2e140026a6ecf6c3e392206
yaping <yaping0816@gmail.com>
2022-11-28 18:41:05 +0000

    finished mini project 2

--- challenges/mini_project2.py
+++ challenges/mini_project2.py
@@ -9,32 +9,30 @@ app = Flask(__name__)
 # list of characters name for users to choose from
 characterlist = []
 # call simpsons api here so I don't need to cal api and get different data every time when i go to the quotes endpoint
-data=requests.get('https://thesimpsonsquoteapi.glitch.me/quotes?count=10').json()
+data=requests.get('https://thesimpsonsquoteapi.glitch.me/quotes?count=5').json()
+
 
 for obj in data:
     character = obj["character"]
     characterlist.append(character)
+# remove the duplicate names from the list
+characterlist = list(dict.fromkeys(characterlist))
 # shuffle the character names inside of the list
 random.shuffle(characterlist)
      
 index =0 # track the current position
 rightcharacter="" # track the right answer
 righturl="" # track the url of the character's image
-quote=""
+quote="" #track the current quote
 
-@app.route("/yaping") # if someone goes to 127.0.0.1:2224/yaping
+# This is the home page to start the game (127.0.0.1:2224/home)
+@app.route("/home") 
 def hello_world():
-    return "Welcome to The Simpsons's quotes\n"
+    return render_template("homepage.html")
 
+# Whoever go to /quotes endpoint can cheat because he can see all of the quotes with the characters
 @app.route("/quotes")
 def display_quotes():
-    # quotes.clear() #clear the data every time when you refreash
-    # for obj in data:
-    #     quote = obj["quote"]
-    #     character = obj["character"]
-    #     newquote =f"{quote} --- {character}"
-    #     quotes.append(newquote)
-    # return quotes
     return data
 
 @app.route("/quotes/<num>")
@@ -45,34 +43,38 @@ def display_quote(num):
     global quote
     num = int(num)
     index = num
-    rightcharacter = data[num]["character"]
-    quote = data[num]["quote"]
-    righturl = data[num]["image"]
-    # return quotes[num]
-    return render_template("guessquote.html",quote = quote, list = characterlist)
+    if num < len(data):
+        rightcharacter = data[num]["character"]
+        quote = data[num]["quote"]
+        righturl = data[num]["image"]
+        return render_template("guessquote.html",quote = quote, list = characterlist)
+    elif num == len(data):
+        return render_template("winthegame.html")
+    else:
+        return "Something went wrong!"
 
 @app.route("/quotes/checkanswer",methods=["POST","GET"])
 def checktheanswer():
     if request.method == "POST":
-        if request.form.get("nm"): # if nm was assigned via the POST
-            character = request.form.get("nm") # grab the value of nm from the POST
-        else: # if a user sent a post without nm then assign value defaultuser
-            character = "defaultuser"
-    # GET would likely come from a user interacting with a browser
-    elif request.method == "GET":
-        if request.args.get("nm"): # if nm was assigned as a parameter=value
-            character = request.args.get("nm") # pull nm from localhost:5060/login?nm=larry
-        else: # if nm was not passed...
-            character = "defaultuser"
+        character = request.form.get("nm") 
     
-    if character.upper() == rightcharacter.upper():
+    elif request.method == "GET":
+        character = request.args.get("nm")
+
+    # check whether user didn't type anything
+    if len(character) == 0:
+        return render_template("emptyinput.html", num=index)
+    # if user typed a right answer
+    elif character.upper() == rightcharacter.upper():
         num = index+1
         return redirect(url_for("display_quote", num=num))
     else:
+    #if user typed a wrong answer
+        random.shuffle(data)
         return render_template("wrongguess.html", name=character,quote=quote, link=righturl,character=rightcharacter)
 
             
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=2224) # runs the application
-   # app.run(host="0.0.0.0", port=2224, debug=True) # DEBUG MODE
+   app.run(host="0.0.0.0", port=2224)
+  
